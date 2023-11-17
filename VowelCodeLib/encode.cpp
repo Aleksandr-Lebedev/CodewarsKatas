c2d5c9e75f4c5cbd99b2815eb7335b1b67000ed8
Aleksandr Lebedev <aleksandr.lebedev.it@gmail.com>
2023-11-17 19:26:46 +0200

    EncodeTable refactoring.

--- VowelCodeLib/encode.cpp
+++ VowelCodeLib/encode.cpp
@@ -6,35 +6,47 @@ namespace
 	class EncodeTable
 	{
 	public:
+		using Entry = std::pair<char, char>;
+
 		static char encode_char(char ch) noexcept
 		{
-			const auto entry_it = std::find_if(encode_table.cbegin(), encode_table.cend(),
-						[ch](const auto& entry) { return entry.first == ch; });
-
-			if (entry_it == encode_table.cend())
+			const auto entry = find_entry(ch);
+			if (entry == empty_entry)
 				return ch;
 			else
-				return entry_it->second;
+				return entry.second;
 		}
 
 		static char decode_char(char ch) noexcept
 		{
-			const auto entry_it = std::find_if(encode_table.cbegin(), encode_table.cend(),
-				[ch](const auto& entry) { return entry.second == ch; });
-
-			if (entry_it == encode_table.cend())
+			const auto entry = find_entry(ch);
+			if (entry == empty_entry)
 				return ch;
 			else
-				return entry_it->first;
+				return entry.first;
 		}
 	private:
-		static constexpr std::array<std::pair<char, char>, 5> encode_table {
-				std::make_pair('a', '1'),
-				std::make_pair('e', '2'),
-				std::make_pair('i', '3'),
-				std::make_pair('o', '4'),
-				std::make_pair('u', '5'),
-			};
+		static Entry find_entry(char ch) noexcept
+		{
+			const auto entry_it = std::find_if(encode_table.cbegin(), encode_table.cend(),
+				[ch](const auto& entry) { return entry.first == ch or entry.second == ch; });
+
+			if (entry_it == encode_table.cend())
+				return empty_entry;
+			else
+				return *entry_it;
+		}
+
+	private:
+		static constexpr std::array<Entry, 5> encode_table {
+			std::make_pair('a', '1'),
+			std::make_pair('e', '2'),
+			std::make_pair('i', '3'),
+			std::make_pair('o', '4'),
+			std::make_pair('u', '5'),
+		};
+
+		static constexpr Entry empty_entry{ '\0', '\0' };
 	};
 }
 
