5bfcc6c19d49ade8307fb69d80cd4e9e319fd1a3
Aleksandr Lebedev <aleksandr.lebedev.it@gmail.com>
2023-11-17 19:06:53 +0200

    Create EncodeTable class

--- VowelCodeLib/encode.cpp
+++ VowelCodeLib/encode.cpp
@@ -3,42 +3,38 @@
 
 namespace
 {
-	constexpr std::array<std::pair<char, char>, 5> ENCODE_TABLE {
-		std::make_pair('a', '1'),
-		std::make_pair('e', '2'),
-		std::make_pair('i', '3'),
-		std::make_pair('o', '4'),
-		std::make_pair('u', '5'),
-	};
-
-	class Encoder
+	class EncodeTable
 	{
 	public:
-		char operator()(char ch)
+		static char encode_char(char ch) noexcept
 		{
-			const auto entry_it = std::find_if(ENCODE_TABLE.cbegin(), ENCODE_TABLE.cend(),
-				[ch](const auto& entry) { return entry.first == ch; });
+			const auto entry_it = std::find_if(encode_table.cbegin(), encode_table.cend(),
+						[ch](const auto& entry) { return entry.first == ch; });
 
-			if (entry_it == ENCODE_TABLE.cend())
+			if (entry_it == encode_table.cend())
 				return ch;
 			else
 				return entry_it->second;
 		}
-	};
 
-	class Decoder
-	{
-	public:
-		char operator()(char ch)
+		static char decode_char(char ch) noexcept
 		{
-			const auto entry_it = std::find_if(ENCODE_TABLE.cbegin(), ENCODE_TABLE.cend(),
+			const auto entry_it = std::find_if(encode_table.cbegin(), encode_table.cend(),
 				[ch](const auto& entry) { return entry.second == ch; });
 
-			if (entry_it == ENCODE_TABLE.cend())
+			if (entry_it == encode_table.cend())
 				return ch;
 			else
 				return entry_it->first;
 		}
+	private:
+		static constexpr std::array<std::pair<char, char>, 5> encode_table {
+				std::make_pair('a', '1'),
+				std::make_pair('e', '2'),
+				std::make_pair('i', '3'),
+				std::make_pair('o', '4'),
+				std::make_pair('u', '5'),
+			};
 	};
 }
 
@@ -49,16 +45,14 @@ namespace VowelCodeLib
 	std::string encode(const std::string& str)
 	{
 		std::string encoded{};
-		Encoder encoder;
-		std::transform(str.cbegin(), str.cend(), std::back_inserter(encoded), encoder);
+		std::transform(str.cbegin(), str.cend(), std::back_inserter(encoded), &EncodeTable::encode_char);
 		return encoded;
 	}
 
 	std::string decode(const std::string& str)
 	{
 		std::string decoded{};
-		Decoder decoder;
-		std::transform(str.cbegin(), str.cend(), std::back_inserter(decoded), decoder);
+		std::transform(str.cbegin(), str.cend(), std::back_inserter(decoded), &EncodeTable::decode_char);
 		return decoded;
 	}
 }
