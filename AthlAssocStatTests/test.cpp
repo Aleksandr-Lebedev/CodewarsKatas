6a8c1160bf567f8b86bf969587401349c188537b
Aleksandr Lebedev <aleksandr.lebedev.it@gmail.com>
2024-04-03 14:18:27 +0300

    Add AthlAssocStatTests
    - Add new unit test project.
    - Add error handling unit tests.

---
+++ AthlAssocStatTests/test.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "athl_assoc_stat_api.h"
+#include "parse_error.h"
+
+TEST(TestErrorHandling, TestRaceResultIsNullptr) {
+	ASSERT_THROW(calculate_race_statistics(nullptr, 10, nullptr, 0), std::invalid_argument);
+}
+
+TEST(TestErrorHandling, TestRaceResultStringLengthIsZero) {
+	std::string dummy{ 10, 'a' };
+	ASSERT_THROW(calculate_race_statistics(dummy.data(), 0, nullptr, 0), std::length_error);
+}
+
+TEST(TestErrorHandling, TestParseError) {
+	std::string dummy{ 10, 'a' };
+	ASSERT_THROW(calculate_race_statistics(dummy.data(), dummy.length(), nullptr, 0), athl_assoc::ParseError);
+}
