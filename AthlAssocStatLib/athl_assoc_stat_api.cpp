7e4f6ad9b178634e701d608dd5379613e70d98b6
Aleksandr Lebedev <aleksandr.lebedev.it@gmail.com>
2024-04-03 15:58:23 +0300

    Add check for a null terminator presence in a string

--- AthlAssocStatLib/athl_assoc_stat_api.cpp
+++ AthlAssocStatLib/athl_assoc_stat_api.cpp
@@ -4,13 +4,36 @@
 #include "calculate_stats.h"
 #include "parse_error.h"
 
+namespace
+{
+	inline int search_null_term(const char* const cstr, int max_offset)
+	{
+		int offset = 0;
+		for (; offset < max_offset; ++offset)
+		{
+			if (cstr[offset] == '\0')
+			{
+				break;
+			}
+		}
+		return offset;
+	}
+}
+
 namespace athl_assoc::api
 {
 	int calculate_race_statistics(const char* const race_results_cstr, char* race_stats_out_buf, int race_stats_out_buf_length)
 	{
+		const auto max_results_cstr_length = MAX_RACE_RESULT_STRING_CHARACTERS + 1;
+
 		if (race_results_cstr == nullptr)
 		{
 			throw std::invalid_argument{"Argument 'race_results_cstr' is a null pointer"};
+		}
+
+		if (search_null_term(race_results_cstr, max_results_cstr_length + 1) > max_results_cstr_length)
+		{
+			throw std::invalid_argument{"Argument 'race_results_cstr' is too long or invalid"};
 		}
 
 		return 0;
--- AthlAssocStatTests/test.cpp
+++ AthlAssocStatTests/test.cpp
@@ -8,6 +8,12 @@ TEST(TestErrorHandling, TestRaceResultIsNullptr) {
 	ASSERT_THROW(calculate_race_statistics(nullptr, nullptr, 0), std::invalid_argument);
 }
 
+TEST(TestErrorHandling, TestTooLongRaceResultsString) {
+	const auto dummy_str_length = static_cast<std::string::size_type>(race_results_str_max_length() * 2);
+	std::string dummy(dummy_str_length, 'a');
+	ASSERT_THROW(calculate_race_statistics(dummy.c_str(), nullptr, 0), std::invalid_argument);
+}
+
 /*
 TEST(TestErrorHandling, TestParseError) {
 	std::string dummy{ 10, 'a' };
