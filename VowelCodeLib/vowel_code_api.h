9407f9de14b4b2b8cc1dd02af292467b7d6c4b72
Aleksandr Lebedev <aleksandr.lebedev.it@gmail.com>
2024-04-08 12:02:57 +0300

    Remove extern C declarations

--- VowelCodeLib/vowel_code_api.h
+++ VowelCodeLib/vowel_code_api.h
@@ -10,10 +10,7 @@
 
 // https://www.codewars.com/kata/53697be005f803751e0015aa/train/cpp
 
-extern "C"
-{
-	int VOWELCODELIB_API encode_vwc(const char* const origin, int origin_size, char* encoded, int encoded_size);
+int VOWELCODELIB_API encode_vwc(const char* const origin, int origin_size, char* encoded, int encoded_size);
 
-	int VOWELCODELIB_API decode_vwc(const char* const encoded, int encoded_size, char* decoded, int decoded_size);
-}
+int VOWELCODELIB_API decode_vwc(const char* const encoded, int encoded_size, char* decoded, int decoded_size);
 
