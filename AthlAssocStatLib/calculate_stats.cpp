e201ab3b16231e7824d8bf2cd519c3aaca28866f
Aleksandr Lebedev <aleksandr.lebedev.it@gmail.com>
2024-05-17 13:47:04 +0300

    Implement 'generate_time_results' function

--- AthlAssocStatLib/calculate_stats.cpp
+++ AthlAssocStatLib/calculate_stats.cpp
@@ -5,23 +5,31 @@
 
 namespace
 {
-	
+	std::vector<athl_assoc::TimeResult> generate_time_results(const std::string& race_results)
+	{
+		using athl_assoc::TimeResult;
+		constexpr int MATCH_GROUP = 1;
+
+		static const auto re = std::regex("(?:,\\w?)?(\\d+\\|[0-5]\\d\\|[0-5]\\d)");
+
+		auto iter = std::sregex_token_iterator(race_results.cbegin(), race_results.cend(), re, MATCH_GROUP);
+
+		std::vector<TimeResult> run_results;
+		std::for_each(iter, std::sregex_token_iterator{}, [&run_results](auto& match) {
+				run_results.push_back(TimeResult::from_str(match.str()));
+		});
+		return run_results;
+	}
 }
 
 namespace athl_assoc
 {
 	std::string calculate_statistics(const std::string& race_results)
 	{
-		constexpr int MATCH_GROUP = 1;
+		const auto run_results = generate_time_results(race_results);
 
-		static const auto re = std::regex("(?:,\\w?)?(\\d+\\|[0-5]\\d\\|[0-5]\\d)");
 
-		std::vector<TimeResult> run_results;
-		for (auto iter = std::sregex_token_iterator(race_results.cbegin(),
-			race_results.cend(), re, MATCH_GROUP); iter != std::sregex_token_iterator{}; ++iter)
-		{
-			run_results.push_back(TimeResult::from_str(iter->str()));
-		}
+
 		return std::string("abc");
 	}
 }
