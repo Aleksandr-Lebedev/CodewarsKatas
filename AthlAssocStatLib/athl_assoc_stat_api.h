75693b13633919025637c0cb8026e6aa406f4219
Aleksandr Lebedev <aleksandr.lebedev.it@gmail.com>
2024-06-13 12:25:46 +0300

    Solution and projects description

--- AthlAssocStatLib/athl_assoc_stat_api.h
+++ AthlAssocStatLib/athl_assoc_stat_api.h
@@ -3,13 +3,8 @@
 #include "library_declspec.h"
 #include "return_codes.h"
 
-// https://www.codewars.com/kata/55b3425df71c1201a800009c
-
 /// @brief Calculate race statistics.
 ///
-/// If the output c-string is not large enough to contain the entire calculation result
-/// then the result string will be truncated.
-/// 
 /// @param[in] race_results_cstr a pointer to a race results c-string
 /// @param[out] race_stats_out_buf a pointer to an allocated c-string for the calculation results or a null pointer
 /// @param[in,out] race_stats_out_buf_length a pointer to the variable length of the output buffer.
---
+++ Readme.md
@@ -0,0 +1,6 @@
+
+## Solution description
+This solution contains small libraries along with tests designed to solve various problems that seemed interesting to me.
+There are two main projects in this solution, **AthlAssocStatLib** and **VowelCodeLib**. Inside the project directories you will find a detailed description of the task.
+
+*The tasks were taken from the site www.codewars.com.*
